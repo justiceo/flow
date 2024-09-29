4c668855a9f8d9d385c4eee3e7a5a907c266477a
Justice Ogbonna <justiceo@users.noreply.github.com>
2024-09-29 00:15:15 -0700

    Add readme and package.json

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flow
+
+A simple API that enhances your LLM integration with automated logging, jest-like testing, on-demand evals, with meaningful and sliceable analytics.
--- src/flow.js
+++ src/flow.js
@@ -3,7 +3,7 @@
 import fs from "fs/promises";
 import path from "path";
 import os from "os";
-import { ChatGptLog } from "./chatgpt-log";
+import { ChatGptLog } from "./chatgpt-log.js";
 
 const LogEntryType = Object.freeze({
   PROMPT: "prompt",
